edb7620e5481c00077bf02e5ae97a86ec1d4a5bd
George Burgyan <gburgyan@vec.com>
2023-10-15 10:18:44 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go Quickgraph Samples
+
+This contains a simple application that exercises some of the features of the library.
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gburgyan/go-quickgraph"
+	"github.com/gin-gonic/gin"
+)
+
+type GreetingResponse struct {
+	Greeting string
+}
+
+func Greeting(name string) GreetingResponse {
+	return GreetingResponse{
+		Greeting: "Hello, " + name,
+	}
+}
+
+func main() {
+	server := gin.Default()
+	graph := quickgraph.Graphy{}
+	ctx := context.Background()
+
+	graph.RegisterProcessorWithParamNames(ctx, "greeting", Greeting, "name")
+
+	type graphqlRequest struct {
+		Query     string          `json:"query"`
+		Variables json.RawMessage `json:"variables"`
+	}
+
+	server.POST("/graphql", func(c *gin.Context) {
+		// Pull the query and variables from the request.
+		var request graphqlRequest
+		err := c.BindJSON(&request)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		res, err := graph.ProcessRequest(c, request.Query, string(request.Variables))
+		if err != nil {
+			// Log the error here, but the response still has a GraphQL response that can be returned.
+		}
+		// Return the response string.
+		c.String(200, res)
+	})
+	server.GET("/graphql", func(c *gin.Context) {
+		schema, err := graph.SchemaDefinition(ctx)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.String(200, schema)
+	})
+
+	server.Run()
+
+}
