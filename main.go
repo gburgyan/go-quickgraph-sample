e2484c11550a6c622b1c65b8daed05e65c5ca94d
George Burgyan <gburgyan@vec.com>
2023-10-21 15:11:13 -0500

    Update to use the default HTTP handler from graphy.
    Moved the `gin` example to its own file.

--- main.go
+++ main.go
@@ -2,59 +2,30 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"github.com/gburgyan/go-quickgraph"
 	"github.com/gburgyan/go-quickgraph-sample/handlers"
-	"github.com/gin-gonic/gin"
 	"github.com/patrickmn/go-cache"
+	"net/http"
 	"time"
 )
 
 func main() {
-	server := gin.Default()
-	graph := quickgraph.Graphy{}
+
 	ctx := context.Background()
 
+	graph := quickgraph.Graphy{}
 	graph.RegisterQuery(ctx, "greeting", handlers.Greeting, "name")
 
 	handlers.RegisterWidgetHandlers(ctx, &graph)
 
+	// Optional: Set a cache for the graph.
 	graph.RequestCache = &SimpleGraphRequestCache{
 		cache: cache.New(5*time.Minute, 10*time.Minute),
 	}
 
-	type graphqlRequest struct {
-		Query     string          `json:"query"`
-		Variables json.RawMessage `json:"variables"`
+	http.Handle("/graphql", graph.HttpHandler())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
 	}
-
-	server.POST("/graphql", func(c *gin.Context) {
-		// Pull the query and variables from the request.
-		var request graphqlRequest
-		err := c.BindJSON(&request)
-		if err != nil {
-			c.JSON(400, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-		res, err := graph.ProcessRequest(c, request.Query, string(request.Variables))
-		if err != nil {
-			// Log the error here, but the response still has a GraphQL response that can be returned.
-		}
-		// Return the response string.
-		c.String(200, res)
-	})
-	server.GET("/graphql", func(c *gin.Context) {
-		schema, err := graph.SchemaDefinition(ctx)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-		c.String(200, schema)
-	})
-
-	server.Run()
 }
